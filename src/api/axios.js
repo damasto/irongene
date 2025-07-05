040840dbb24707cbf5ddb1fb764090606489a75a
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-07-05 13:06:21 +0200

    set up api to connect to render

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5005",
+    baseURL: "https://irongene-api.onrender.com",
     headers: {
         "Content-Type": "application/json"
     }
